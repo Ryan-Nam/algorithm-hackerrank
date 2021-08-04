78e6dd0d6b801b2fa41860a52534e230a80b3446
Ryan-Nam <alfofh7060@gmail.com>
2021-08-04 23:51:44 +1000

    Add EOF qeustion

--- src/Introduction/End_of_file.java
+++ src/Introduction/End_of_file.java
@@ -1,4 +1,29 @@
+/*
+"In computing, End Of File (commonly abbreviated EOF) is a condition
+in a computer operating system where no more data can be read from a data source."
+
+The challenge here is to read n lines of input until you reach EOF, then number and print all n lines of content.
+Hint: Java's Scanner.hasNext() method is helpful for this problem.
+
+Input Format
+Read some unknown n lines of input from stdin(System.in) until you reach EOF; each line of input contains a non-empty String.
+
+Output Format
+For each line, print the line number, followed by a single space, and then the line content received as input.
+ */
 package Introduction;
 
+import java.util.Scanner;
+
 public class End_of_file {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = 1;
+        while(sc.hasNext()){
+            String str = sc.nextLine();
+            System.out.println(a + " " + str);
+            a++;
+        }
+        sc.close();
+    }
 }
