36e6c93b279e73764cd919a1e682ecb2d721f29e
Ryan-Nam <alfofh7060@gmail.com>
2021-08-07 22:20:31 +1000

    Add fix

--- src/Introduction/Static_Initializer_Block.java
+++ src/Introduction/Static_Initializer_Block.java
@@ -1,7 +1,36 @@
 package Introduction;
 
+import java.util.Scanner;
+
 public class Static_Initializer_Block {
-    public static void main (String[] args){
-        System.out.println("Hi");
+    //Write your code here
+    static boolean flag=false;
+    static int B,H;
+    static{
+        Scanner in= new Scanner(System.in);
+        B=in.nextInt();
+        H=in.nextInt();
+        try{
+            if(B<=0||H<=0)
+                throw new Exception("Breadth and height must be positive");
+            else{
+                flag= true;
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
-}
+
+
+
+    public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+
+    }//end of main
+
+}//end of class
+
